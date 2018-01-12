860069842ec91db3cfdc980c3d6d42053b1bd1db
kentaro <kentaro@kentaro-macbook-air.local>
2018-01-12 09:52:50 +0300

    Initial Commit

---
+++ nogizaka-1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nogizaka-1
+//
+//  Created by kentaro on 2018/01/12.
+//  Copyright © 2018 kentaro kusumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
